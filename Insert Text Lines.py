cee2288d06a9a9b90327f212dde8006dd2e24c21
angelinetipa <angelinetipa@gmail.com>
2023-04-23 11:57:03 +0800

    made while loop to input lines

--- Insert Text Lines.py
+++ Insert Text Lines.py
@@ -1,8 +1,14 @@
-# Create mylife.txt
+# create mylife.txt
 with open("mylife.txt", "a") as file_mylife:
-    # while input is y, loop to first line
+# while input is y, loop to first line
     more_line = ""
     while more_line != "n":
+        # input for text line
         line = input("\nEnter line: ") 
+        # write input to mylife.txt
         file_mylife.write(line)
+        file_mylife.write("\n")
+        # input if need more line
         more_line = input("Are there more lines y/n? ")
+
+# print output
