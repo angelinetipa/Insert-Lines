cf8dbed28f553f9a3c50a587638f483c1ad27eac
angelinetipa <angelinetipa@gmail.com>
2023-04-25 23:59:20 +0800

    change the output

--- Insert Text Lines.py
+++ Insert Text Lines.py
@@ -1,14 +1,11 @@
-# create mylife.txt
+# open mylife.txt[append]
 with open("mylife.txt", "a") as file_mylife:
-# while input is y, loop to first line
+    # while input is yes, loop to first line
     more_line = ""
-    while more_line != "n":
+    while more_line != "no":
         # input for text line
-        line = input("\nEnter line: ") 
+        line = input("\n\033[1m\033[3mEnter line:  \033[0m") 
         # write input to mylife.txt
-        file_mylife.write(line)
-        file_mylife.write("\n")
+        file_mylife.write(line + "\n")
         # input if need more line
-        more_line = input("Are there more lines y/n? ")
-
-# print output
+        more_line = input("\033[1m\033[3mAre there more lines yes/no?  \033[0m")
